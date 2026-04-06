c5990ca2cf44115bdc790cf3b5c7d8ca3311c676
pedro <pedrobritobastos234@gmail.com>
2026-04-06 08:52:24 -0300

    header refatorado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,44 @@
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex, Link } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
     <>
       <nav>
-         <Flex justify="center" gap="1em" lg={{gap: "5em", padding: "1.5em"}} padding="1em" backgroundColor="rgb(70, 65, 64)" width="100vw" position="fixed" zIndex="10">
-            <Link href='#sobre' sm={{fontSize: "18px"}} lg={{fontSize: "24px"}}>Sobre</Link>
-            <Link href='#habilidades' sm={{fontSize: "18px"}} lg={{fontSize: "24px"}}>Habilidades</Link>
-            <Link href='#projetos' sm={{fontSize: "18px"}} lg={{fontSize: "24px"}}>Projetos</Link>
-         </Flex>
+        <Flex
+          justify="center"
+          gap="1em"
+          lg={{ gap: "5em", padding: "1.5em" }}
+          padding="1em"
+          backgroundColor="rgb(70, 65, 64)"
+          width="100vw"
+          position="fixed"
+          zIndex="10"
+        >
+          <Link
+            href="#sobre"
+            sm={{ fontSize: "16px" }}
+            lg={{ fontSize: "20px" }}
+          >
+            Sobre
+          </Link>
+          <Link
+            href="#habilidades"
+            sm={{ fontSize: "16px" }}
+            lg={{ fontSize: "20px" }}
+          >
+            Habilidades
+          </Link>
+          <Link
+            href="#projetos"
+            sm={{ fontSize: "16px" }}
+            lg={{ fontSize: "20px" }}
+          >
+            Projetos
+          </Link>
+        </Flex>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
